5d6e5a486082f546454095e44df6a757eb9e1107
Jonathan Rovner <jonrovner@gmail.com>
2021-01-30 19:39:55 -0300

    continue to build portfolio website

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,14 @@
 const codingCard = document.querySelector('.coding');
 const codingWorks = document.querySelector('.works');
+const cardContent = document.querySelector('.cardContent');
 
 codingCard.addEventListener('click', function(){
     if (codingWorks.classList.contains('hidden')){
+        cardContent.classList.add('hidden');
         codingWorks.classList.remove('hidden')
     }  else {
         codingWorks.classList.add('hidden')
+        cardContent.classList.remove('hidden')
     }
 
 })
